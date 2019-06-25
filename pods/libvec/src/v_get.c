aa1faabc25b1a3e4b00ba06af09f1823fdda9f0c
glegendr <glegendr@student.42.fr>
2019-06-25 18:37:37 +0200

    pc1 is finished

--- pods/libvec/src/v_get.c
+++ pods/libvec/src/v_get.c
@@ -6,7 +6,7 @@
 /*   By: glegendr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/12 22:03:59 by glegendr          #+#    #+#             */
-/*   Updated: 2019/06/24 11:15:59 by glegendr         ###   ########.fr       */
+/*   Updated: 2019/06/25 13:29:03 by glegendr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void		*v_get(t_vec *vec, int index)
 {
 	void *pos;
 
-	if (index >= vec->private_elem_nb)
-		return (NULL);
 	pos = vec->private_content + index * vec->private_elem_size;
 	return (pos);
 }
--- src/des-ecb.c
+++ src/des-ecb.c
@@ -2,7 +2,7 @@
 #include <libft.h>
 
 
-
+#include <ft_printf.h>
 #include <stdio.h>
 
 const char *get_pwd(void)
@@ -18,38 +18,164 @@ const char *get_pwd(void)
 	return ret;
 }
 
+void		get_true_pwd(const char *pwd, uint8_t *ret)
+{
+	int i;
+	uint8_t in;
+
+	i = 0;
+	while (i < 16)
+	{
+		in = pwd[i];
+		if (in >= 'a' && in <= 'f')
+			in -= 97 - 10;
+		else if (in >= 'A' && in <= 'F')
+			in -= 65 - 10;
+		else if (in >= '0' && in <= '9')
+			in -= 48;
+		ret[i / 2] = ret[i / 2] << 4;
+		ret[i / 2] = ret[i / 2] | in;
+		++i;
+	}
+}
+
+/*
+** 57 - 1   8
+** 49 - 2   8
+** 41 - 3   8
+** 33 - 4   8
+** 25 - 5   8
+** 17 - 6   8
+** 9 - 7    8
+** 1 - 8    END
+**
+** 58 - 9   8
+** 50 - 10  8
+** 42 - 11  8
+** 34 - 12  8
+** 26 - 13  8
+** 18 - 14  8
+** 10 - 15  8
+** 2  - 16  8
+**
+** 59 - 17  8
+** 51 - 18  8
+** 43 - 19  8
+** 35 - 20  8
+** 27 - 21  8
+** 19 - 22  8
+** 11 - 23  8
+** 3  - 24
+**
+** 60 - 25  8
+** 52 - 26  8
+** 44 - 27  8
+** 36 - 28
+**
+** 63 - 29  8
+** 55 - 30  8
+** 47 - 31  8
+** 39 - 32  8
+** 31 - 33  8
+** 23 - 34  8
+** 15 - 35  8
+** 7  - 36
+**
+** 62 - 37  8
+** 54 - 38  8
+** 46 - 39  8
+** 38 - 40  8
+** 30 - 41  8
+** 22 - 42  8
+** 14 - 43  8
+** 6  - 44  8
+**
+** 61 - 45  8
+** 53 - 46  8
+** 45 - 47  8
+** 37 - 48  8
+** 29 - 49  8
+** 21 - 50  8
+** 13 - 51  8
+** 5  - 52
+**
+** 28 - 53  8
+** 20 - 54  8
+** 12 - 55  8
+** 4  - 56  8
+**
+*/
+
+
+void		bit(uint8_t *ret, int i, uint8_t *str, int pos)
+{
+	uint8_t x;
+	int bit;
+
+	x = pos - pos / 8 * 8;
+	x = 1 << (8 - x);
+	bit = (str[pos / 8] & x) >> (8 - (pos - pos / 8 * 8));
+	ret[i / 8] = ret[i / 8] << 1;
+	ret[i / 8] = ret[i / 8] | bit;
+}
+
+void		in_str(uint8_t *str, uint8_t *ret)
+{
+	str[0] = ret[0];
+	str[1] = ret[1];
+	str[2] = ret[2];
+	str[3] = ret[3];
+	str[4] = ret[4];
+	str[5] = ret[5];
+	str[6] = ret[6];
+}
+
+void		pc1(uint8_t *str)
+{
+	uint8_t ret[7];
+	int x;
+	int loop;
+
+	loop = 0;
+	for (int i = 0; i < 56; ++i)
+	{
+		if (i == 28)
+			loop = 8;
+		else if (i == 52)
+			loop = -27;
+		if (i < 28)
+		{
+			if (i % 8 == 0)
+				++loop;
+			x = 56 + loop - (i % 8) * 8;
+		}
+		else
+		{
+			if ((i + 4) % 8 == 0)
+				--loop;
+			x = 56 + loop - ((i + 4) % 8) * 8;
+		}
+		bit(ret, i, str, x);
+	}
+	in_str(str, ret);
+}
+
 void		des_ecb(t_hash *hash)
 {
 	t_ops ops = hash->ops;
 	uint8_t *str;
 	t_vec *vec;
+	uint8_t true_pwd[8];
 
 	if (!ops.pwd)
 		ops.pwd = get_pwd();
-	write(1, ops.pwd, ft_strlen(ops.pwd));
 
+	printf("\n");
 	vec = v_get(&hash->str, 0);
-	write(1, v_raw(vec), v_size(vec));
 	str = v_raw(vec);
-	printf("HELL1\n");
-	str[0] ^= ops.pwd[0];
-	printf("HELL2\n");
-	str[1] ^= ops.pwd[1];
-	printf("HELL3\n");
-	str[2] ^= ops.pwd[2];
-	printf("HELL4\n");
-	str[3] ^= ops.pwd[3];
-	printf("HELL5\n");
-	str[4] ^= ops.pwd[4];
-	printf("HELL6\n");
-	str[5] ^= ops.pwd[5];
-	printf("HELL7\n");
-	str[6] ^= ops.pwd[6];
-	printf("HELL8\n");
-	str[7] ^= ops.pwd[7];
-	printf("HELL9\n");
-	write(1, str, 8);
-
+	get_true_pwd(ops.pwd, true_pwd);
+	pc1(str);
+	printf("%x %x %x %x %x %x %x\n", str[0], str[1], str[2], str[3], str[4], str[5], str[6]);
 //	read_file(&tab, 0, false);
 //	into_vec(&tab.folder, NULL);
 }
